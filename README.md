5b7c6094c68432a1b1b251573cfd6b2597648250
me <me@MacBook-Pro-me.local>
2022-05-22 20:00:32 +0300

    completed

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# api_yamdb
-api_yamdb
+Клонировать репозиторий git clone git@github.com:neukuhren/api_yamdb.git
+cd api_yamdb
+Cоздать и активировать виртуальное окружение: python -m venv venv
+source venv/scripts/activate
+python -m pip install --upgrade pip
+Установить зависимости из файла requirements.txt: pip install -r requirements.txt
+Выполнить миграции: python manage.py migrate
+Запустить проект: python manage.py runserver
+В проекте доступны следующие эндпоинты: http://127.0.0.1:8000/api/v1/auth/signup/ - Получение кода подверждения на email
+{ "email": "string", "username": "string" }
+Клонирование базы
+прейти в Python Shell командой
+python manage.py shell
+импорт необходимых модулей
+import os inport csv
+установить путь до базы
+path = "с:/../api_yamdb/static/data"
+Сделать импорт модели: from reviews.models import Genre, Category, Title, Review, Comment, GenreTitle from users.models import User
+Последовательно для каждой модели запустить цикл записи данных:
+на примере модели пользователей.
+with open('users.csv') as csvfile: reader = csv.DictReader(csvfile) for row in reader: p = User(id=row['id'], username=row['username'], email=row['email'], role=row['role'], bio=row['bio'], first_name=row['first_name'], last_name=row['last_name']) p.save()
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +23,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'django_filters',
+    'reviews',
+    'api',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -101,3 +109,31 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
+
+AUTH_USER_MODEL = 'users.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
